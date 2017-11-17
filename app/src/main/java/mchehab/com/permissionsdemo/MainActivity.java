43a9f46a156332975fb50cdf92981661dd4f5bb8
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-17 18:00:05 +0200

    Added permission handling using RxPermissions library

--- app/src/main/java/mchehab/com/permissionsdemo/MainActivity.java
+++ app/src/main/java/mchehab/com/permissionsdemo/MainActivity.java
@@ -2,73 +2,55 @@ package mchehab.com.permissionsdemo;
 
 import android.Manifest;
 import android.app.AlertDialog;
-import android.content.Intent;
-import android.net.Uri;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-import permissions.dispatcher.NeedsPermission;
-import permissions.dispatcher.OnNeverAskAgain;
-import permissions.dispatcher.OnPermissionDenied;
-import permissions.dispatcher.RuntimePermissions;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
-@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
+
+    private String[] permissions = {Manifest.permission.READ_CONTACTS, Manifest.permission
+            .WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION};
+    private RxPermissions rxPermissions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rxPermissions = new RxPermissions(this);
+
         Button button = findViewById(R.id.button);
         button.setOnClickListener(e-> {
-            MainActivityPermissionsDispatcher.requestPermissionsWithPermissionCheck(this);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions();
+            }
         });
     }
 
-    @NeedsPermission({Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission
-            .ACCESS_FINE_LOCATION, Manifest.permission.READ_CONTACTS})
-    void requestPermissions(){
-        //do whatever you want here
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
-    }
-
-    @OnPermissionDenied({Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission
-            .ACCESS_FINE_LOCATION, Manifest.permission.READ_CONTACTS})
-    void permissionsDenied(){
-        new AlertDialog.Builder(this)
-                .setTitle("Permission denied")
-                .setMessage("Enable permissions")
-                .setPositiveButton("Allow", (dialog, which) -> MainActivityPermissionsDispatcher
-                        .requestPermissionsWithPermissionCheck(MainActivity.this))
-                .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
-                .create()
-                .show();
-    }
-
-    @OnNeverAskAgain({Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission
-            .ACCESS_FINE_LOCATION, Manifest.permission.READ_CONTACTS})
-    void onNeverAskAgain(){
-        new AlertDialog.Builder(this)
-                .setTitle("Title")
-                .setMessage("Message")
-                .setPositiveButton("Ok", (dialog, which) -> {
-                    Intent intent = new Intent();
-                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                    Uri uri = Uri.fromParts("package", getPackageName(), null);
-                    intent.setData(uri);
-                    startActivity(intent);
-                })
-                .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss()).create()
-                .show();
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestPermissions(){
+        rxPermissions.request(permissions)
+            .subscribe(granted -> {
+                if(granted){
+    //                        code goes here
+                }else{
+                    for(String permission : permissions){
+                        if(shouldShowRequestPermissionRationale(permission)){
+                            new AlertDialog.Builder(this)
+                                    .setTitle("Error")
+                                    .setMessage("Your message here")
+                                    .setPositiveButton("Allow", (dialog, which) -> requestPermissions())
+                                    .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
+                                    .create()
+                                    .show();
+                            return;
+                        }
+                    }
+                }
+            });
     }
 }
